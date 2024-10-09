b4173ca8b2f96fc9707cb7b62ae075b250a17872
BayernMuller <cjjunk29@gmail.com>
2024-10-09 19:02:31 +0900

    feat: add support for math rendering with remark-math and rehype-katex

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,9 +5,15 @@ import mdx from '@astrojs/mdx'
 import preact from '@astrojs/preact'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
+import remarkMath from 'remark-math'
+import rehypeKatex from "rehype-katex";
 
 export default defineConfig({
   integrations: [mdx(), preact(), react(), sitemap()],
   base: `/good-code-bad-code/`,  // GitHub Pages 프로젝트 경로로 수정
   site: `https://theintrance.github.io/good-code-bad-code`,  // GitHub Pages 배포 URL로 수정
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  }
 })
