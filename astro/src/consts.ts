a6ec33d89cd6353043b46ba4db15eee7c9d1a089
BayernMuller <cjjunk29@gmail.com>
2024-10-28 15:37:27 +0900

    feat: add chapter on code quality goals in Korean

--- astro/src/consts.ts
+++ astro/src/consts.ts
@@ -38,6 +38,7 @@ export const SIDEBAR: Sidebar = {
   ko: {
     'Chapter 1': [
       { text: '코드는 어떻게 소프트웨어가 되는가', link: '/ko/./chapter1/1_1_코드는_어떻게_소프트웨어가_되는가' },
+      { text: '코드 품질의 목표', link: '/ko/./chapter1/1_2_코드_품질의_목표' },
     ],
   }
 }
